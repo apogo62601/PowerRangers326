3bef34d98667d46268a0af128e1f1343b95dce9c
JJacobs2001 <103562585+JJacobs2001@users.noreply.github.com>
2022-11-17 13:38:51 -0500

    Update choose_adventure.py

--- choose_adventure.py
+++ choose_adventure.py
@@ -1,1 +1,35 @@
+if __name__ == "__main__":
 
+def Pandas_Points():
+# METHOD: Reading Points File Data
+"""Using PANDAS to organize and pull the points data
+# import pandas as pd
+# df = pd.read_csv('file.csv')
+"""
+
+def __Repr__():
+# METHOD: __Repr__ for formal strings
+"""Using magic method to formally represent
+"""
+
+def Counter():
+# METHOD: Counter for Points (add/subtract)
+"""
+"""
+
+
+def Conditional_Choices():
+# METHOD: Printing prompted if storychoice#1 or storychoice#2 is chosen
+"""
+"""
+
+
+
+def Set_Story_Intersections():
+# METHOD: Checking if story choices have the same text-outcome and/or points amount
+"""
+"""
+
+
+def With_Conditions():
+# METHOD: Open text file with prompts and then put conditions in based on user picks

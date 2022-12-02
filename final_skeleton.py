f024eecf36d25ed21b0cbeb74411e473f9acaf30
Aelina Pogosian <60406142+apogo62601@users.noreply.github.com>
2022-12-02 14:00:42 -0500

    Update final_skeleton.py

--- final_skeleton.py
+++ final_skeleton.py
@@ -5,47 +5,71 @@ def Pandas_Points():
 # METHOD: Reading Points File Data
 """Using PANDAS to organize and pull the points data
 
+Args:
+  csv_file(str): csv file
+Attributes:
+  
+Returns:
+  Returns user point value as an int
+"""
+
 import pandas as pd
 df = pd.read_csv("game.csv")
 
-"""
 
 def __Repr__():
 # METHOD: __Repr__ for formal strings
 """Using magic method to formally represent
-
-
-
+Returns: 
+  str: string representation
+  
 """
 
 def Counter():
 # METHOD: Counter for Points (add/subtract)
-"""
-
-
-
+""" Counts the amount of points the user receives.
+Args:
+  points(int): Amount of points the user has.
+  point_change(int): Amount of points the user gains/ loses.
+Returns:
+  The sum of points that is awarded to the user as an int.
 """
 
 
 def Conditional_Choices():
 # METHOD: Printing prompted if storychoice#1 or storychoice#2 is chosen
-"""
-
-
-
+"""Checks between routes that are choosable and adds to history list of choices made
+Args:
+  Storychoice1 (str): A user route option
+  Storychoice2 (str): Another user route option
+  Storychoice3 (str): Optional parameter for certain choice prompts
+  
+Attributes:
+  self.list (list): Storage of previous choices made
+RaiseError:
+  Error if chosen user option is not valid. Loops back to prior options
 """
 
 
 
 def Set_Story_Intersections():
 # METHOD: Checking if story choices have the same text-outcome and/or points amount
+""" Check overall user route to determine if a mojority of choices were good/bad to determine how the day was
+Args:
+  person_day (str): description of the persons day
+ 
+ Returns: 
+  print statement regarding the quality of their day as a string
+  
+  
 """
-
-
-
-"""
-
 
 def With_Conditions():
 # METHOD: Open text file with prompts and then put conditions in based on user picks
-
+"""
+Args:
+  path(str): file path to text prompts
+  condition1(str): condition user picks
+Side effects:
+  Prints to stdout
+"""

aca3938f361a9df25e970bc71f752be7f16f1d94
abelnega <34039756+abelnega@users.noreply.github.com>
2022-12-12 14:04:42 -0500

    Update choose_adventure.py

--- choose_adventure.py
+++ choose_adventure.py
@@ -96,70 +96,7 @@ get_points('skip class')
 
 
 
-def Conditional_Choices(Storychoice1, Storychoice2, Storychoice = None):
-# METHOD: Printing prompted if storychoice#1 or storychoice#2 is chosen
-# Aelina
-"""Checks between routes that are choosable and adds to history list of choices made
-Args:
-  Storychoice1 (str): A user route option
-  Storychoice2 (str): Another user route option
-  Storychoice3 (str): Optional parameter for certain choice prompts
-  
-Attributes:
-  self.list (list): Storage of previous choices made
 
-RaiseError:
-  Error if chosen user option is not valid. Loops back to prior options
-  
-"""
-with open("adventure.txt") as file:
-
- for x in file:
-  print(f"Your choice is {"""choice"""}, you currently have {"""points"""} points.")
-
-
-
-def Set_Story_Intersections(person_day):
-# METHOD: Checking if story choices have the same text-outcome and/or points amount
-# Justin
-""" Check overall user route to determine if a mojority of choices were good/bad to determine how the day was
-Args:
-  person_day (str): description of the persons day
- 
- Returns: 
-  print statement regarding the quality of their day as a string
-"""
-
-    if points >= 60:
-        print("It was a good day")
-        
-    if points < 60:
-        print("It was a bad day")
-      
-    bad_choices_set = {"hello", "bye"}
-    
-    good_choices_set = {"hello", "cool", "bye"}
-    
-    total_set = bad_choices_set & good_choices_set
-    
-    Counter(points, point_change)
-    
-    # List comprehension for finding point amount needed to 
-    threshold_adder = [x+1 for x in bad_choices_set if x < 60]
-    
-    
-
-def Counter(points, point_change):
-# METHOD: Counter for Points (add/subtract)
-# Abel
-""" Counts the amount of points the user receives.
-Args:
-  points(int): Amount of points the user has.
-  point_change(int): Amount of points the user gains/ loses.
-
-Returns:
-  The sum of points that is awarded to the user as an int.
-"""
 
 def parse_args(arglist):
 #METHOD: Creates command-line interface for the user
